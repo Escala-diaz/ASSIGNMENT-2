9bff9cdbf65caef311e258e7cd3769bb12fe0b15
Escala-diaz <zaidm7074@gmail.com>
2025-05-19 14:01:23 +0530

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,7 +1,7 @@
 text=(input('Enter the number: '))
 even =int(2)
 
-if int(text)%even== 0:
+if int(text)%even == 0:
     print(""+ text +" is an even number")
 else:
     print(""+ text +" is an odd number")
